d91a14dfcf989ca262ae10fc511831a77993811f
LioneL Christopher Chetty <lionel@getbuild.today>
2018-02-12 22:54:10 +0200

    Initial

---
+++ ExampleMultitenancy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExampleMultitenancy.Startup))]
+namespace ExampleMultitenancy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
